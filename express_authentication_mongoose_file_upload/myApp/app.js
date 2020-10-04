e5b3922067793218036afa171837e233caacc8d5
LalithK90 <asakahatapitiya@gmail.com>
2020-10-04 08:36:24 +0530

    Upload Router was configure on app.js

--- express_authentication_mongoose_file_upload/myApp/app.js
+++ express_authentication_mongoose_file_upload/myApp/app.js
@@ -48,8 +48,6 @@ app.all('*', (req, res, next) => {
     res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
   }
 });
-//upload router 
-app.use('/imageUpload',uploadRouter);
 app.use(passport.initialize());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -68,7 +66,8 @@ app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions',promotionsRouter);
 app.use('/leaders',leadersRouter);
-
+//upload router 
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
